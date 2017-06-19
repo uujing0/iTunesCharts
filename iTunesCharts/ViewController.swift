638565524d344e871193bac75555315f66c93100
Jang Yujin <you9010@uangel.com>
2017-06-20 00:53:35 +0900

    first commit

---
+++ iTunesCharts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iTunesCharts
+//
+//  Created by Jang Yujin on 2017. 6. 20..
+//  Copyright © 2017년 you9010. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
