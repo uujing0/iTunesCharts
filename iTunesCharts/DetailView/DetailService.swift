cbbe898b1e05b20968c93ce81dce47117725bc3a
you9010 <you9010@uangel.com>
2017-06-23 14:15:59 +0900

    Added model, cell, viewModel related DetailViewController.

---
+++ iTunesCharts/DetailView/DetailService.swift
@@ -0,0 +1,9 @@
+//
+//  DetailService.swift
+//  iTunesCharts
+//
+//  Created by Jang Yujin on 2017. 6. 23..
+//  Copyright © 2017년 you9010. All rights reserved.
+//
+
+import Foundation
---
+++ iTunesCharts/DetailView/DetailTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DetailTableViewCell.swift
+//  iTunesCharts
+//
+//  Created by Jang Yujin on 2017. 6. 23..
+//  Copyright © 2017년 you9010. All rights reserved.
+//
+
+import UIKit
+
+class DetailTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
